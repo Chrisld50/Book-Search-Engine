74f860ecb444ccecf4ae7a7851261c6639d3b058
Chrisld50 <chrisld50@yahoo.com>
2023-01-11 21:38:12 -0600

    arranged code in mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,15 @@
 import { gql } from '@apollo/client';
 
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!){
+    login(email: $email, password: $password){
+        token
+        user {
+            _id
+            username
+        }
+    }
+}`
 
 export const ADD_USER = gql`
 mutation addUser($username: String!, $email: String!, $password: String!){
@@ -22,21 +32,6 @@ mutation addUser($username: String!, $email: String!, $password: String!){
     }
 }`;
 
-
-
-
-export const USER_LOGIN = gql`
-mutation login($email: String!, $password: String!){
-    login(email: $email, password: $password){
-        token
-        user {
-            _id
-            username
-        }
-    }
-}`
-
-
 export const SAVE_BOOK = gql`
 mutation saveBook($newBook: InputBook!){
     saveBook(newBook: $newBook){
@@ -53,8 +48,6 @@ mutation saveBook($newBook: InputBook!){
         }
     }
 }`;
-
-
 
 export const REMOVE_BOOK = gql`
 mutation removeBook($bookId: ID!){
