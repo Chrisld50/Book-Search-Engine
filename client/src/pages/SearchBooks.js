f1132044bb7a6520e440f6e0b39c66c0c4b71659
Chrisld50 <chrisld50@yahoo.com>
2023-01-11 22:16:20 -0600

    deleted api folder and controller folder, updated pages

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
 const SearchBooks = () => {
@@ -65,7 +66,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+       const { data } = await saveBook({
+        variables: {newBook: {...bookToSave}}
+       }) ;
 
       if (!response.ok) {
         throw new Error('something went wrong!');
